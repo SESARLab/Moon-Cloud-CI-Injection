1dd734e117c8b73482c6da21f510c64efd12ded2
Alex Della Bruna <alex.dellabruna@studenti.unimi.it>
2024-02-29 12:08:53 +0100

    Updated script

--- mooncloud_utils.py
+++ mooncloud_utils.py
@@ -2,6 +2,7 @@
 import sys
 import time
 import requests
+import argparse
 
 class MoonCloudUtils:
     mooncloud_api_login_endpoint="https://api.v2.moon-cloud.eu/user/login-token/"
@@ -15,77 +16,92 @@ class MoonCloudUtils:
             "username":user,
             "password":password
         })
-        if response.status_code==200:
-            response_obj=response.json()
-            return response_obj["token"]
-        else:
-            print(f"Error occured, Status Code:{response.status_code}, Reason:{response.reason}")
-            exit(1)
+        response.raise_for_status()
+        return response.json()["token"]
 
     @staticmethod
-    def startEvaluation(token,uer_id,wait_result):
+    def startEvaluation(token,uer_id,wait_result) -> bool:
+        execution_result=None
         initial_result_number=None
         if wait_result:
             initial_result_number=MoonCloudUtils.getEvaluationResult(token=token,uer_id=uer_id)[1]
         response=requests.put(url=MoonCloudUtils.mooncloud_api_evaluation_start_endpoint.format(uer_id=uer_id),headers={
             "Content-Type":"application/json",
             "Authorization":"Token "+token})
-        if response.status_code==200:
-            response_obj=response.json()
-            if wait_result:
-                new_result=False
-                tmp_result=None
-                t_counter=0
-                while not new_result and t_counter<MoonCloudUtils.max_attempt_number:
-                    tmp_result,tmp_result_number=MoonCloudUtils.getEvaluationResult(token=token,uer_id=uer_id)
-                    if tmp_result_number==(initial_result_number+1):
-                        new_result=True
-                    print("Waiting for evaluation completion...")
-                    time.sleep(10)
-                    t_counter=t_counter+1                
-                if t_counter>=MoonCloudUtils.max_attempt_number:
-                    print("Max attempt number exceeded")
-                    exit(1)
-                return {
-                    "status":response_obj["result"],
-                    "result":tmp_result["result"],
-                }
-            else:
-                return {
-                    "status":response_obj["result"],
-                }
+        response.raise_for_status()
+        print("Evaluation started correctly")
+        start_evaluation_response_obj=response.json()
+        if wait_result:
+            new_result_flag=False
+            tmp_result=None
+            t_counter=0
+            while not new_result_flag and t_counter<MoonCloudUtils.max_attempt_number:
+                tmp_result,tmp_result_number=MoonCloudUtils.getEvaluationResult(token=token,uer_id=uer_id)
+                if tmp_result_number==(initial_result_number+1):
+                    new_result_flag=True
+                print("Waiting for evaluation completion...")
+                time.sleep(10)
+                t_counter=t_counter+1
+            if t_counter>=MoonCloudUtils.max_attempt_number:
+                raise TimeoutError("Max attempt number exceeded")
+            execution_result={
+                "evaluation_started":True if start_evaluation_response_obj["result"]=="started" else False,
+                "evaluation_result":True if tmp_result["result"]==True else False
+            }
         else:
-            print(f"Error occured, Status Code:{response.status_code}, Reason:{response.reason}")
-            exit(1)
+            execution_result={
+                "evaluation_started":True if start_evaluation_response_obj["result"]=="started" else False
+            }
+        return execution_result
     
     @staticmethod
     def getEvaluationResult(token,uer_id):
         response=requests.get(url=MoonCloudUtils.mooncloud_api_evaluation_endpoint.format(uer_id=uer_id),headers={
             "Content-Type":"application/json",
             "Authorization":"Token "+token})
-        if response.status_code==200:
-            response_obj=response.json()
-            return response_obj,len(response_obj["tests"][0]["execution_results"])
-        else:
-            print(f"Error occured, Status Code:{response.status_code}, Reason:{response.reason}")
-            exit(1)
+        response.raise_for_status()
+        response_obj=response.json()
+        return response_obj,len(response_obj["tests"][0]["execution_results"])
+
+def exitWithError(error_str):
+    print("Error occured: "+error_str, file=sys.stderr)
+    exit(1)
 
 if __name__ == "__main__":
-    # python mooncloud_utils.py get-token --user <user> --password <password>
-    if len(sys.argv)==6 and sys.argv[1]=="get-token" and sys.argv[2]=="--username" and sys.argv[4]=="--password":
-        token=MoonCloudUtils.getToken(user=sys.argv[3],password=sys.argv[5])
-        print(token)
-    # python mooncloud_utils.py start-evaluation --token <token> --uer-id <uer_id>
-    elif len(sys.argv)==6 and sys.argv[1]=="start-evaluation" and sys.argv[2]=="--token" and sys.argv[4]=="--uer-id":
-        response=MoonCloudUtils.startEvaluation(token=sys.argv[3],uer_id=sys.argv[5],wait_result=False)
-        assert response["status"]=="started", "Something went wrong"
-        print("Evaluation started")
-    # python mooncloud_utils.py start-evaluation --token <token> --uer-id <uer_id> --wait-result
-    elif len(sys.argv)==7 and sys.argv[1]=="start-evaluation" and sys.argv[2]=="--token" and sys.argv[4]=="--uer-id" and sys.argv[6]=="--wait-result":
-        response=MoonCloudUtils.startEvaluation(token=sys.argv[3],uer_id=sys.argv[5],wait_result=True)
-        assert response["status"]=="started", "Something went wrong"
-        print("Evaluation result:"+("true" if response["result"]==True else "false"))
-        assert response["result"], "Evaluation result false"
-    else:
-        print("Params not valid")
-        exit(1)
+    parser = argparse.ArgumentParser(description="Get token by providing username and password")
+    subparsers = parser.add_subparsers(title="commands", dest="command")
+
+    # get-token option
+    get_token_parser = subparsers.add_parser("get-token", help="Get token")
+    get_token_parser.add_argument("--username", required=True, help="Username")
+    get_token_parser.add_argument("--password", required=True, help="Password")
+
+    # start-evaluation option
+    start_evaluation_parser = subparsers.add_parser("start-evaluation", help="Get token")
+    start_evaluation_parser.add_argument("--token", required=True, help="Token")
+    start_evaluation_parser.add_argument("--uer-id", required=True, help="UER ID")
+    start_evaluation_parser.add_argument("--wait-result", required=False, action="store_true", help="Wait for evaluation result")
+
+    args = parser.parse_args()
+
+    if args.command == "get-token":
+        try:
+            print(MoonCloudUtils.getToken(user=args.username,password=args.password))
+        except Exception as e:
+            exitWithError(str(e))
+    elif args.command == "start-evaluation":
+        if not args.wait_result:
+            try:
+                response=MoonCloudUtils.startEvaluation(token=args.token,uer_id=args.uer_id,wait_result=False)
+                assert response["evaluation_started"], "An error occured during evaluation starting"
+                print(response)
+            except Exception as e:
+                exitWithError(str(e))
+        else:
+            try:
+                response=MoonCloudUtils.startEvaluation(token=sys.argv[3],uer_id=sys.argv[5],wait_result=True)
+                assert response["evaluation_started"], "An error occured during evaluation starting"
+                assert response["evaluation_result"], "Evaluation failed"
+                print(response)
+            except Exception as e:
+                exitWithError(str(e))
